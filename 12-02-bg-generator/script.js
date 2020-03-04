91ff9909b68faa91ed3c74df5e1025cfc9cbf96a
William Porter <wmporter@users.noreply.github.com>
2020-03-04 14:30:03 -0800

    Add code to randomly set background color

--- 12-02-bg-generator/script.js
+++ 12-02-bg-generator/script.js
@@ -1,6 +1,7 @@
 var css = document.querySelector('h3')
 var color1 = document.getElementById('color1')
 var color2 = document.getElementById('color2')
+var rando = document.getElementById('rando')
 var body = document.querySelector('body')
 
 function updateGradient() {
@@ -8,5 +9,21 @@ function updateGradient() {
     body.style.background = cssString
     css.textContent = cssString + ';'
 }
+
+function setRandomBackground() {
+    const hex = '0123456789abcdef';
+    let c1 = '#', c2 = '#';
+    for(let i = 0; i < 6; i++) {
+        c1 += hex[Math.floor(16 * Math.random())]
+    }
+    for(let j = 0; j < 6; j++) {
+        c2 += hex[Math.floor(16 * Math.random())]
+    }
+    color1.value = c1;
+    color2.value = c2;
+    updateGradient()
+}
+
 color1.addEventListener('input', updateGradient)
 color2.addEventListener('input', updateGradient)
+rando.addEventListener('click', setRandomBackground)
